8a17cb2d75acd8842f84570a674d21b206173dd2
gs.song <rhrnrkd135@gmail.com>
2023-01-04 18:32:52 +0900

    [ + ] vue 프로젝트 세팅

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div id="app">
+		<AppHeaderVue />
+		<img alt="Vue logo" src="./assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppHeaderVue from '@/components/coomon/AppHeader.vue';
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+	name: 'App',
+	components: {
+		HelloWorld,
+		AppHeaderVue,
+	},
+	created() {
+		console.log('hi');
+	},
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/coomon/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<ul>
+			<li>기본 header</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+ul,
+ol {
+	list-style: none;
+}
+</style>
